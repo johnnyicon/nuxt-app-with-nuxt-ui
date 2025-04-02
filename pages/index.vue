2b70d3b09a079a92fe899aa481bb5e7943ef5d18
John Gonzales <john@merkhetventures.com>
2025-04-02 09:30:35 -0400

    @nuxt/ui configuration
    Followed installation instructions here: https://ui.nuxt.com/getting-started/installation/nuxt

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="flex flex-col gap-4">
+      <h1>Hello World</h1>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+    </div>
+  </div>
+</template>
